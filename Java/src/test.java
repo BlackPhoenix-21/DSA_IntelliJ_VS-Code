8c7ad77d68116344603f42b0276f398004a1c6e7
Sören <blackphoenix21.dark@gmail.com>
2025-01-15 12:55:36 +0100

    fix

--- Java/src/test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-
-        System.out.println("Hello");
-        System.out.println("enens du dawg");
-        System.out.println("kaka pupu");
-
-    }
-}
