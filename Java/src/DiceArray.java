fb434bc32f21bd421c716b1de68afed0d4ad636a
Sören <blackphoenix21.dark@gmail.com>
2025-01-17 09:55:58 +0100

    DiceArray format-fix

--- Java/src/DiceArray.java
+++ Java/src/DiceArray.java
@@ -8,17 +8,25 @@ public class DiceArray {
         int[] counter = { 0, 0, 0, 0, 0, 0 };
 
         for (int i = 1; i <= 1000; i++) {
+
             int zahl = rd.nextInt(6) + 1;
+
             counter[zahl - 1]++;
         }
+
         int max = 0;
+
         for (int i = 0; i < counter.length; i++) {
+
             if (counter[i] > max) {
                 max = counter[i];
             }
+
             System.out.println("Die Zahl " + (i + 1) + " wurde " + counter[i] + " gewürfelt");
         }
+
         for (int i = 0; i < counter.length; i++) {
+
             if (counter[i] == max) {
                 System.out.println("Meiste gewürfelte Zahl ist " + i + " mit " + max + " würfen");
             }
