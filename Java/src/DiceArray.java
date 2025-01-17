060f89a9babcd041fef209985a4069d809ebda6b
Sören <blackphoenix21.dark@gmail.com>
2025-01-17 11:19:33 +0100

    ArraySort

--- Java/src/DiceArray.java
+++ Java/src/DiceArray.java
@@ -31,5 +31,38 @@ public class DiceArray {
                 System.out.println("Meiste gewürfelte Zahl ist " + i + " mit " + max + " würfen");
             }
         }
+
+        int[] sortiert = ArrySort(counter);
+
+        for (int i = 0; i < counter.length; i++) {
+            System.out.print(counter[i] + ", ");
+        }
+
+        System.out.println(" ");
+
+        for (int i = 0; i < sortiert.length; i++) {
+            System.out.print(sortiert[i] + ", ");
+        }
+    }
+
+    private static int[] ArrySort(int[] zahlen) {
+
+        int[] numbers = new int[zahlen.length];
+
+        for (int i = 0; i < zahlen.length; i++) {
+            numbers[i] = zahlen[i];
+        }
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] > numbers[j]) {
+                    int hold = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = hold;
+                }
+            }
+        }
+
+        return numbers;
     }
 }
