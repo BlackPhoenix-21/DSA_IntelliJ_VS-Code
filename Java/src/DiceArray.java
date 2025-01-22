278de1662bf8f2d50b110b4791398f33a36efad4
Sören <blackphoenix21.dark@gmail.com>
2025-01-22 14:31:50 +0100

    bubble

--- Java/src/DiceArray.java
+++ Java/src/DiceArray.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class DiceArray {
@@ -28,41 +29,53 @@ public class DiceArray {
         for (int i = 0; i < counter.length; i++) {
 
             if (counter[i] == max) {
-                System.out.println("Meiste gewürfelte Zahl ist " + i + " mit " + max + " würfen");
+                System.out.println("Meiste gewürfelte Zahl ist " + (i + 1) + " mit " + max + " würfen");
             }
         }
 
         int[] sortiert = ArraySort(counter);
+        int[] sortiert2 = ArraySort2(counter);
 
-        for (int i = 0; i < counter.length; i++) {
-            System.out.print(counter[i] + ", ");
-        }
-
-        System.out.println(" ");
-
-        for (int i = 0; i < sortiert.length; i++) {
-            System.out.print(sortiert[i] + ", ");
-        }
+        System.out.println(Arrays.toString(counter));
+        System.out.println(Arrays.toString(sortiert));
+        System.out.println(Arrays.toString(sortiert2));
     }
 
     private static int[] ArraySort(int[] zahlen) {
 
-        int[] numbers = new int[zahlen.length];
-
-        for (int i = 0; i < zahlen.length; i++) {
-            numbers[i] = zahlen[i];
-        }
+        int[] numbers = zahlen.clone();
 
         for (int i = 0; i < numbers.length - 1; i++) {
             for (int j = i + 1; j < numbers.length; j++) {
+                // System.out.println("Vergleiche " + numbers[i] + " mit " + numbers[j]);
                 if (numbers[i] > numbers[j]) {
                     int hold = numbers[i];
                     numbers[i] = numbers[j];
                     numbers[j] = hold;
+                    // System.out.println("Tausche " + numbers[i] + " mit " + numbers[j]);
                 }
             }
         }
+        return numbers;
+    }
 
+    private static int[] ArraySort2(int[] zahlen) {
+        int[] numbers = zahlen.clone();
+        boolean swapped = true;
+        while (swapped) {
+            swapped = false;
+            for (int i = 0; i < numbers.length - 1; i++) {
+                System.out.println("Vergleiche " + numbers[i] + " mit " + numbers[i + 1]);
+                if (numbers[i] > numbers[i + 1]) {
+                    swapped = true;
+                    int hold = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = hold;
+                    System.out.println("Tausche " + numbers[i] + " mit " + numbers[i + 1]);
+                }
+
+            }
+        }
         return numbers;
     }
 }
