cda5397490efdea785c4700b83ac7749f0589cfa
Sören Irlenbusch <blackphoenix21.dark@gmail.com>
2025-02-14 09:24:09 +0100

    Fibo

--- Java/src/FibonacciFolge.java
+++ Java/src/FibonacciFolge.java
@@ -1,15 +1,18 @@
-import java.util.Scanner;
 
 public class FibonacciFolge {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long eingabe = sc.nextInt();
-        sc.close();
-        for (int i = 1; i < eingabe; i++) {
+        for (int i = 1; i < 10; i++) {
             long aus = Rechner(i);
             System.out.println(i + " : " + aus);
         }
-
+        FibonacciSequence(50);
+        long aus = 0;
+        try {
+            aus = FibonacciNumber(-1);
+        } catch (IllegalArgumentException e) {
+            aus = FibonacciNumber(10);
+        }
+        System.out.println(aus);
     }
 
     public static long Rechner(long n) {
@@ -18,5 +21,37 @@ public class FibonacciFolge {
         } else {
             return Rechner(n - 1) + Rechner(n - 2);
         }
+    }
+
+    private static long FibonacciNumber(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Das Argument dasrf nicht negativ sein!");
+        }
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        return FibonacciNumber(n - 2) + FibonacciNumber(n - 1);
+    }
+
+    private static void FibonacciSequence(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Das Argument dasrf nicht negativ sein!");
+        }
+        if (n == 0) {
+            System.out.print("0");
+            return;
+        }
+        long prex = 1;
+        long prevprevx = 1;
+        System.out.print("0, 1");
+
+        for (int i = 2; i <= n; i++) {
+            long newx = prevprevx + prex;
+            System.out.print(", " + newx);
+            prevprevx = prex;
+            prex = newx;
+        }
+
     }
 }
