4a6e49c2cf83750723c08f022990a11e34647d3b
Sören Irlenbusch <blackphoenix21.dark@gmail.com>
2025-03-12 11:41:59 +0100

    UnitTest

--- Java/src/Vector3Test.java
+++ Java/src/Vector3Test.java
@@ -1,7 +1,13 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class Vector3Test {
-
+    @Test
     public void lenghtTest() {
-
+        Vector3 v = new Vector3(3, 4, 5);
+        assertEquals(7.07106, v.getLength(), 0.0001f);
+        v = new Vector3(0, 0, 0);
+        assertEquals(0, v.getLength(), 0);
     }
 }
